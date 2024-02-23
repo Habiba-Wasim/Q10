46efee291fa1e23ca54d6b225cad857f9264f7cb
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:04:06 +0500

    Step10 Of Typescript
    Step10 Of Typescript

---
+++ Q10.js
@@ -0,0 +1,18 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Addition
+let a = 10;
+let b = 20;
+console.log(`${a} + ${b} = ${a + b}`);
+//subtraction
+let c = 10;
+let d = 20;
+console.log(`${c} - ${d} = ${c - d}`);
+//Multiplication
+let e = 10;
+let f = 20;
+console.log(`${e} * ${f} = ${e * f}`);
+//Division
+let g = 10;
+let h = 20;
+console.log(`${h} / ${g} = ${h / g}`);
---
+++ Q10.ts
@@ -0,0 +1,20 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Addition
+let a:number = 10
+let b:number = 20
+console.log(`${a} + ${b} = ${a + b}`);
+
+//subtraction
+let c: number = 10
+let d: number = 20
+console.log(`${c} - ${d} = ${c - d}`);
+
+//Multiplication
+let e: number = 10
+let f: number = 20
+console.log(`${e} * ${f} = ${e * f}`);
+
+//Division
+let g: number = 10
+let h: number = 20
+console.log(`${h} / ${g} = ${h / g}`);
